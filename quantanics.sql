dfe532e8380d57c11fad6691507573e279b5e04d
Jaxtonkavi <115336296+Jaxtonkavi@users.noreply.github.com>
2023-05-16 22:52:43 +0530

    sql file add
    quantanics

--- quantanics.sql
+++ quantanics.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2023 at 08:39 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 16, 2023 at 11:13 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `quantanis`
+-- Database: `quantanics`
 --
 
 -- --------------------------------------------------------
@@ -33,14 +33,14 @@ CREATE TABLE `admin_user` (
   `emp_name` varchar(98) DEFAULT NULL,
   `dept` varchar(90) DEFAULT NULL,
   `designation` varchar(90) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin_user`
 --
 
 INSERT INTO `admin_user` (`user_id`, `password`, `emp_name`, `dept`, `designation`) VALUES
-('9100', '123', 'sir', 'cse', 'web');
+('EMP1001', 'nk1234', 'StrategyNaveen', 'BE CSE', 'FULL STACK DEVELOPER');
 
 -- --------------------------------------------------------
 
@@ -52,7 +52,17 @@ CREATE TABLE `domain_table` (
   `domain_id` varchar(90) NOT NULL,
   `domain_name` varchar(98) DEFAULT NULL,
   `domain_status` varchar(90) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `domain_table`
+--
+
+INSERT INTO `domain_table` (`domain_id`, `domain_name`, `domain_status`) VALUES
+('1', 'Web Development', '1'),
+('2', 'IOT Engineer', '1'),
+('3', 'Mobile Development', '1'),
+('4', 'Web Full Stack', '1');
 
 -- --------------------------------------------------------
 
@@ -62,8 +72,8 @@ CREATE TABLE `domain_table` (
 
 CREATE TABLE `intern_table` (
   `intern_id` varchar(99) NOT NULL,
-  `regno` varchar(90) NOT NULL,
   `sname` varchar(90) NOT NULL,
+  `regno` varchar(99) NOT NULL,
   `dept` varchar(90) NOT NULL,
   `year` varchar(98) NOT NULL,
   `email_id` varchar(98) NOT NULL,
@@ -77,20 +87,22 @@ CREATE TABLE `intern_table` (
   `end_date` date NOT NULL,
   `domain` varchar(98) NOT NULL,
   `registeration_status` int(90) NOT NULL,
+  `intern_status` varchar(99) NOT NULL,
   `reject_reason` varchar(98) NOT NULL,
   `allowance` int(90) NOT NULL,
   `fees` int(90) NOT NULL,
-  `dob` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `dob` date NOT NULL,
+  `payment_status` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `intern_table`
 --
 
-INSERT INTO `intern_table` (`intern_id`, `regno`, `sname`, `dept`, `year`, `email_id`, `clg_name`, `mobile`, `resume`, `bonafide`, `id_card`, `profile`, `start_date`, `end_date`, `domain`, `registeration_status`, `reject_reason`, `allowance`, `fees`, `dob`) VALUES
-('QAIN1001', '910018104304', 'strategistnaveen', 'cse', '4th year', 'naveenkumar709434@gmail.com', 'anna university', 9942933262, 'images.jpg', 'images (1).jpg', 'logo (2).jpg', 'logo_old.png', '2023-03-10', '2023-04-13', 'Web', 1, '', 0, 0, '2000-04-18'),
-('QAIN1002', '9100', 'nithesh', 'cse', '4th', 'nitheshwaran003@gmail.com', 'aurcm', 8123456789, 'jennie.xlsx', 'infosysqn.py', '1669833857260.jpg', '1669833857260.jpg', '2023-04-10', '2023-04-10', 'Web', 1, '', 0, 0, '2023-04-10'),
-('QAIN1003', '91001', 'nithesh', 'cse', '4th', 'nitheshwaran004@gmail.com', 'aurcm', 8123456789, '1669833857260.jpg', '1669833857260.jpg', '1669833857260.jpg', '1669833857260.jpg', '2023-04-11', '2023-04-11', 'Web', 0, '', 0, 0, '2023-04-11');
+INSERT INTO `intern_table` (`intern_id`, `sname`, `regno`, `dept`, `year`, `email_id`, `clg_name`, `mobile`, `resume`, `bonafide`, `id_card`, `profile`, `start_date`, `end_date`, `domain`, `registeration_status`, `intern_status`, `reject_reason`, `allowance`, `fees`, `dob`, `payment_status`) VALUES
+('QAIN1001', 'Naveen Kumar', '', 'BE CSE', '4th year', 'naveenkumar709434@gmail.com', 'Anna University Regional Campus', 6379617916, '', '', '', 'empty', '0000-00-00', '0000-00-00', '4', 1, 'paid', '', 0, 0, '2000-04-18', '2000'),
+('QAIN1002', 'navin', '', 'cse', '4th year', 'naveekumarnk18420@gmail.com', 'aurcm', 6379617916, '', '', '', 'empty', '0000-00-00', '0000-00-00', '4', 1, 'free', '', 0, 0, '2023-05-04', ''),
+('QAIN1003', 'strategy', '', 'cse', '4th year', 'strategy@gmail.com', 'AURCM', 9942933262, '', '', '', 'empty', '0000-00-00', '0000-00-00', '2', 2, '', '                    sfsdf', 0, 0, '2023-05-14', '');
 
 -- --------------------------------------------------------
 
@@ -99,25 +111,43 @@ INSERT INTO `intern_table` (`intern_id`, `regno`, `sname`, `dept`, `year`, `emai
 --
 
 CREATE TABLE `message_table` (
-  `id` int(90) NOT NULL,
-  `intern_id` varchar(98) DEFAULT NULL,
-  `intern_name` varchar(98) DEFAULT NULL,
-  `intern_msg` text DEFAULT NULL,
-  `domain_name` varchar(98) NOT NULL,
-  `profile_name` varchar(98) NOT NULL,
-  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(99) NOT NULL,
+  `mid` varchar(99) NOT NULL,
+  `mname` varchar(99) NOT NULL,
+  `msg` text NOT NULL,
+  `mdomain` varchar(99) NOT NULL,
+  `mprofile` varchar(99) NOT NULL,
+  `messager_status` varchar(99) NOT NULL,
+  `mstatus` varchar(99) NOT NULL,
+  `last_updated_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `message_table`
 --
 
-INSERT INTO `message_table` (`id`, `intern_id`, `intern_name`, `intern_msg`, `domain_name`, `profile_name`, `time_stamp`) VALUES
-(1, 'QAIN1001', 'strategistnaveen', 'hi', 'Web', 'logo_old.png', '2023-04-02 14:25:55'),
-(2, 'QAIN1001', 'strategistnaveen', 'nk', 'Web', 'logo_old.png', '2023-04-02 14:43:01'),
-(3, 'QAIN1001', 'strategistnaveen', 'hi hello ', 'Web', 'logo_old.png', '2023-04-03 02:06:49'),
-(4, 'QAIN1002', 'nithesh', 'hai', 'Web', '1669833857260.jpg', '2023-04-10 04:20:10'),
-(5, 'QAIN1002', 'nithesh', 'ff', 'Web', '1669833857260.jpg', '2023-04-10 04:27:12');
+INSERT INTO `message_table` (`id`, `mid`, `mname`, `msg`, `mdomain`, `mprofile`, `messager_status`, `mstatus`, `last_updated_on`) VALUES
+(1, 'QAIN1001', 'Naveen Kumar', 'hi', '4', 'empty', 'intern', '1', '2023-05-14 06:04:25'),
+(2, 'QAIN1001', 'Naveen Kumar', 'welcome Naveen', '4', 'empty', 'intern', '1', '2023-05-14 06:19:38'),
+(3, 'EMP1001', 'StrategyNaveen', 'hi welcome to the quantanics', '4', 'empty', 'admin', '1', '2023-05-14 07:16:53'),
+(4, 'EMP1001', 'StrategyNaveen', 'hi', '4', 'empty', 'admin', '1', '2023-05-14 07:18:20'),
+(5, 'EMP1001', 'StrategyNaveen', 'Good Morining Guy`s', '4', 'empty', 'admin', '1', '2023-05-14 07:19:16'),
+(6, 'EMP1001', 'StrategyNaveen', 'Good Afternoon', '4', 'empty', 'admin', '1', '2023-05-14 07:20:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_table`
+--
+
+CREATE TABLE `payment_table` (
+  `intern_id` varchar(99) NOT NULL,
+  `paid_amount` int(99) NOT NULL,
+  `remaining_amount` int(99) NOT NULL,
+  `total_payment` int(99) NOT NULL,
+  `payment_proof` varchar(99) NOT NULL,
+  `last_updated_on` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -141,15 +171,7 @@ CREATE TABLE `task_table` (
   `intern_id` varchar(98) DEFAULT NULL,
   `check_in` time NOT NULL,
   `check_out` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `task_table`
---
-
-INSERT INTO `task_table` (`id`, `tdate`, `task`, `task_desc`, `task_status`, `domain`, `correction_count`, `assigned_by`, `completion_file`, `remark`, `comment`, `last_updated_by`, `intern_id`, `check_in`, `check_out`) VALUES
-(1, '2023-04-10', 'q', 'q', 'q', 'q', 'q', 'q', 'q', 'q', 'q', 'q', 'QAIN1002', '14:05:14', '24:05:14'),
-(145, NULL, 'Query Builders', 'To insert the data in db', 'Successfully inserted', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'QAIN1002', '00:05:00', '01:05:00');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -172,15 +194,13 @@ ALTER TABLE `domain_table`
 --
 ALTER TABLE `intern_table`
   ADD PRIMARY KEY (`intern_id`),
-  ADD UNIQUE KEY `regno` (`regno`),
   ADD UNIQUE KEY `email_id` (`email_id`);
 
 --
 -- Indexes for table `message_table`
 --
 ALTER TABLE `message_table`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `intern_id` (`intern_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `task_table`
@@ -197,23 +217,17 @@ ALTER TABLE `task_table`
 -- AUTO_INCREMENT for table `message_table`
 --
 ALTER TABLE `message_table`
-  MODIFY `id` int(90) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `task_table`
 --
 ALTER TABLE `task_table`
-  MODIFY `id` int(98) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+  MODIFY `id` int(98) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `message_table`
---
-ALTER TABLE `message_table`
-  ADD CONSTRAINT `message_table_ibfk_1` FOREIGN KEY (`intern_id`) REFERENCES `intern_table` (`intern_id`);
 
 --
 -- Constraints for table `task_table`
